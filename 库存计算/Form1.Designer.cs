ef22dcefaf8a9d21ee324181451747798d43aafb
MoondanceZ <QYK_zZ@hotmail.com>
2018-07-26 22:23:22 +0800

    init

--- 库存计算/Form1.Designer.cs
+++ 库存计算/Form1.Designer.cs
@@ -28,12 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOrder = new System.Windows.Forms.TextBox();
+            this.btnOpenOrder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInventory = new System.Windows.Forms.TextBox();
+            this.btnOpenInventory = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "订单Excel：";
+            // 
+            // txtOrder
+            // 
+            this.txtOrder.Location = new System.Drawing.Point(72, 15);
+            this.txtOrder.Name = "txtOrder";
+            this.txtOrder.Size = new System.Drawing.Size(278, 21);
+            this.txtOrder.TabIndex = 1;
+            // 
+            // btnOpenOrder
+            // 
+            this.btnOpenOrder.Location = new System.Drawing.Point(356, 13);
+            this.btnOpenOrder.Name = "btnOpenOrder";
+            this.btnOpenOrder.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenOrder.TabIndex = 2;
+            this.btnOpenOrder.Text = "打开";
+            this.btnOpenOrder.UseVisualStyleBackColor = true;
+            this.btnOpenOrder.Click += new System.EventHandler(this.btnOpenOrder_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "输出路径：";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblOutput.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblOutput.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblOutput.Location = new System.Drawing.Point(148, 145);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 12);
+            this.lblOutput.TabIndex = 6;
+            this.lblOutput.Click += new System.EventHandler(this.lblOutput_Click);
+            // 
+            // btnTransfer
+            // 
+            this.btnTransfer.Location = new System.Drawing.Point(356, 162);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(75, 23);
+            this.btnTransfer.TabIndex = 7;
+            this.btnTransfer.Text = "计算";
+            this.btnTransfer.UseVisualStyleBackColor = true;
+            this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(72, 111);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(276, 21);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(356, 111);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnOutput.TabIndex = 6;
+            this.btnOutput.Text = "选择目录";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "库存Excel：";
+            // 
+            // txtInventory
+            // 
+            this.txtInventory.Location = new System.Drawing.Point(72, 56);
+            this.txtInventory.Name = "txtInventory";
+            this.txtInventory.Size = new System.Drawing.Size(278, 21);
+            this.txtInventory.TabIndex = 3;
+            // 
+            // btnOpenInventory
+            // 
+            this.btnOpenInventory.Location = new System.Drawing.Point(356, 54);
+            this.btnOpenInventory.Name = "btnOpenInventory";
+            this.btnOpenInventory.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenInventory.TabIndex = 4;
+            this.btnOpenInventory.Text = "打开";
+            this.btnOpenInventory.UseVisualStyleBackColor = true;
+            this.btnOpenInventory.Click += new System.EventHandler(this.btnOpenInventory_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(23, 145);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 12);
+            this.lblInfo.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(441, 219);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnTransfer);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnOpenInventory);
+            this.Controls.Add(this.txtInventory);
+            this.Controls.Add(this.btnOpenOrder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOrder);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "库存计算";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOrder;
+        private System.Windows.Forms.Button btnOpenOrder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnTransfer;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInventory;
+        private System.Windows.Forms.Button btnOpenInventory;
+        private System.Windows.Forms.Label lblInfo;
     }
 }
 
--- 库存计算/Form1.cs
+++ 库存计算/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -14,6 +15,195 @@ namespace 库存计算
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void btnOpenOrder_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Excel文件(*.xls*)|*.xls*";
+            dialog.RestoreDirectory = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] pathArray = dialog.FileNames;
+                if (pathArray.Length > 1 || pathArray.Length == 0)
+                {
+                    MessageBox.Show("请选择一个文件!");
+                }
+                else
+                {
+                    txtOrder.Text = dialog.FileName;
+                }
+            }
+        }
+
+        private void btnOpenInventory_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Excel文件(*.xls*)|*.xls*";
+            dialog.RestoreDirectory = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] pathArray = dialog.FileNames;
+                if (pathArray.Length > 1 || pathArray.Length == 0)
+                {
+                    MessageBox.Show("请选择一个文件!");
+                }
+                else
+                {
+                    txtInventory.Text = dialog.FileName;
+                }
+            }
+        }
+
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnTransfer.Text = "计算中...";
+                lblInfo.Text = "";
+                lblOutput.Text = "";
+                btnOpenOrder.Enabled = false;
+                btnOpenInventory.Enabled = false;
+                btnTransfer.Enabled = false;
+                btnOutput.Enabled = false;
+                txtOrder.Enabled = false;
+                txtInventory.Enabled = false;
+                txtOutput.Enabled = false;
+
+                if (string.IsNullOrEmpty(txtOrder.Text))
+                {
+                    MessageBox.Show("请先选择订单文件!");
+                }
+                if (string.IsNullOrEmpty(txtInventory.Text))
+                {
+                    MessageBox.Show("请先选择库存文件!");
+                }
+                else if (string.IsNullOrEmpty(txtOutput.Text))
+                {
+                    MessageBox.Show("请选择保存路径!");
+                }
+                else
+                {
+                    string outputPath = GetFilePath(txtOutput.Text);
+                    if (!Directory.Exists(outputPath))
+                        Directory.CreateDirectory(outputPath);
+
+                    var orderDt = new ExcelHelper(txtOrder.Text).ExcelToDataTable();
+                    if (orderDt.Rows.Count == 0)
+                    {
+                        MessageBox.Show("订单文件为空!");
+                        return;
+                    }
+                    var inventoryDt = new ExcelHelper(txtInventory.Text).ExcelToDataTable();
+                    if (inventoryDt.Rows.Count == 0)
+                    {
+                        MessageBox.Show("库存文件为空!");
+                        return;
+                    }
+                    orderDt.Columns.Add("是否发货");
+                    inventoryDt.Columns.Add("已发数量", typeof(int));
+                    inventoryDt.Columns.Add("剩余库存", typeof(int));
+
+                    //订单excel排序
+                    var tempOrderDt = orderDt.Clone();
+                    foreach (DataRow item in orderDt.AsEnumerable().OrderBy(m => m.Field<string>("选项代码")).ThenBy(m => Convert.ToDateTime(m.Field<string>("汇款日"))))
+                    {
+                        tempOrderDt.ImportRow(item);
+                    }
+
+                    //保存最终结果
+                    var resultOrderDt = tempOrderDt.Clone();
+                    foreach (DataRow item in tempOrderDt.AsEnumerable())
+                    {
+                        var inventoryRow = inventoryDt.AsEnumerable().FirstOrDefault(m => m.Field<string>("款号") == item["选项代码"].ToString());
+                        if (inventoryRow != null)
+                        {
+                            var totalCount = int.Parse(inventoryRow["数量"].ToString());
+                            var usedCount = String.IsNullOrEmpty(inventoryRow["已发数量"].ToString()) ? 0 : int.Parse(inventoryRow["已发数量"].ToString());
+                            var orderCount = int.Parse(item["数量"].ToString());
+                            if (orderCount <= (totalCount - usedCount))
+                            {
+                                item["是否发货"] = "是(当前库存" + (totalCount - usedCount) + ")";
+                                inventoryRow["已发数量"] = usedCount + orderCount;
+                                inventoryRow["剩余库存"] = totalCount - (usedCount + orderCount);
+                                resultOrderDt.ImportRow(item);
+                            }
+                        }
+                    }
+
+                    var resultInventoryDt = inventoryDt.Clone();
+                    foreach (DataRow inventoryRow in inventoryDt.AsEnumerable())
+                    {
+                        inventoryRow["已发数量"] = String.IsNullOrEmpty(inventoryRow["已发数量"].ToString()) ? 0 : int.Parse(inventoryRow["已发数量"].ToString());
+                        //inventoryRow["剩余库存"] = String.IsNullOrWhiteSpace(inventoryRow["剩余库存"].ToString()) ? int.Parse(inventoryRow["数量"].ToString()) : int.Parse(inventoryRow["剩余库存"].ToString());
+                        resultInventoryDt.ImportRow(inventoryRow);
+                    }
+
+                    if (resultOrderDt.Rows.Count > 0)
+                    {
+                        DataSet ds = new DataSet();
+                        ds.Tables.Add(resultOrderDt);
+                        ds.Tables.Add(resultInventoryDt);
+                        new ExcelHelper(txtOutput.Text).DataSetToExcel(ds, true);
+                        //MessageBox.Show("计算完成");
+                        lblInfo.Text = "计算完成，点击打开：";
+                        lblOutput.Text = txtOutput.Text;
+                    }
+                    else
+                        MessageBox.Show("没有可以发货的款号");
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                btnTransfer.Text = "计算";
+                btnOpenOrder.Enabled = true;
+                btnOpenInventory.Enabled = true;
+                btnTransfer.Enabled = true;
+                btnOutput.Enabled = true;
+                txtOrder.Enabled = true;
+                txtInventory.Enabled = true;
+                txtOutput.Enabled = true;
+                txtOrder.Focus();
+            }
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Excel文件(*.xlsx)|*.xlsx";
+            dialog.RestoreDirectory = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtOutput.Text = dialog.FileName;
+            }
+        }
+
+        private void lblOutput_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", lblOutput.Text);
+        }
+
+        private string GetFilePath(string fullPath)
+        {
+            string[] pathInfo = fullPath.Split('\\');
+            if (pathInfo.Length > 1)
+            {
+                int idx = fullPath.LastIndexOf('\\');
+                string resultStr = fullPath.Substring(0, idx - 0);
+                return resultStr;
+            }
+
+            return "";
         }
     }
 }
